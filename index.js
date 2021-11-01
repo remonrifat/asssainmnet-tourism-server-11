0d2e21ffec29f2ad0fe65d27c47e1c2dcec5c394
unknown <remonrifat2000@gmail.com>
2021-11-01 20:06:06 +0600

    added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,17 +13,29 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tanph.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+
+
 async function run() {
     try {
         await client.connect();
-        const database = client.db("travel_master");
-        const userCollection = database.collection("user");
+        const database = client.db("travelmaster");
+        const usersCollection = database.collection("users");
         // post api
-        app.get('/users', async (req, res) => {
-            const cursor = userCollection.find({});
-            const users = await cursor.toArray();
-            res.send('users');
-        })
+        app.post('/users', async (req, res) => {
+
+
+            const users = {
+                "title": "Cinque Terre ",
+                "imageUrl": "https://media.tacdn.com/media/attractions-splice-spp-360x240/07/3c/5a/45.jpg",
+                "description": "The trip to Cinque Terre was well organized and the guides were professional, fun, informative, friendly, caring, concerned for our safety and very helpful. Both Alberto, our guide, and Diego were fabulous. Having great guides makes all the difference in having a great time when on an"
+            }
+            const result = await usersCollection.insertOne(users);
+            console.log(result);
+
+            // const cursor = usersCollection.find({});
+            // const users = await cursor.toArray();
+            // res.send('users');
+        });
     } finally {
         // await client.close();
     }
@@ -30,16 +43,13 @@ async function run() {
 run().catch(console.dir);
 
 
-
-
-
 app.get('/', (req, res) => {
-    res.send('Ema jon server is running');
+    res.send('Emaftyut jon server is running');
 });
 
 
 app.listen(port, () => {
-    console.log('server runningat port', port);
+    console.log('server running port', port);
 })
 
 
